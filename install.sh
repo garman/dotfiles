873117eed00995e186d8089d68c118e97fe5b255
Daniel Garman <garman@github.com>
2021-09-29 19:08:46 -0700

    Improve bash_profile

--- install.sh
+++ install.sh
@@ -6,13 +6,12 @@ set -x
 
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
-rm -f $HOME/.zshrc
-ln -s $(pwd)/zshrc $HOME/.zshrc
-rm -f $HOME/.bash_profile
-ln -s $(pwd)/bash_profile $HOME/.bash_profile
 ln -s $(pwd)/gitmessage $HOME/.gitmessage
 mkdir -p $HOME/.config/gh
 ln -s $(pwd)/gh_config.yml $HOME/.config/gh/.config.yml
+rm -f $HOME/.zshrc
+ln -s $(pwd)/zshrc $HOME/.zshrc
+ln -s $(pwd)/bash_profile $HOME/.bash_profile
 
 sudo apt-get --assume-yes install silversearcher-ag bat
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
