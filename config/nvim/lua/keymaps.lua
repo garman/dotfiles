38b58a835e6271e8e6c5462f9fd92be12ef0f9f5
Daniel Garman <danielrgarman@gmail.com>
2024-02-24 22:51:56 +0000

    Port tabs, unsuccessfully

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -2,11 +2,18 @@ local map = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 local cmd = vim.cmd
 
+map("n", "<leader><leader>", ":nohl<CR>", default_opts)
+
 map("i", "jk", "<Esc>", {noremap = true})
+
 map("n", ";;", "<cmd>lua require('fzf-lua').files()<CR>", default_opts)
 
-cmd "let g:tmux_navigator_no_mappings = 1"
 map("n", "<C-h>", "<C-W><C-H>", default_opts)
 map("n", "<C-j>", "<C-W><C-J>", default_opts)
 map("n", "<C-k>", "<C-W><C-K>", default_opts)
 map("n", "<C-l>", "<C-W><C-L>", default_opts)
+
+map("n", "<leader>t", "<C-u>tabnew<CR>", default_opts)
+map("n", "<C-t>", "<C-u>tabnew<CR>", default_opts)
+map("n", "<Tab>", "<C-u>tabnext<CR>", default_opts)
+map("n", "<S-Tab>", "<C-u>tabprevious<CR>", default_opts)
