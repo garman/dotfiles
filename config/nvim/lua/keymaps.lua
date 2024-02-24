b83ea0200c5ac76f904fc9f2ba8b581d10fe3938
Daniel Garman <danielrgarman@gmail.com>
2024-02-24 22:43:16 +0000

    Remove the tmux-navigator plugin

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -3,11 +3,10 @@ local default_opts = { noremap = true, silent = true }
 local cmd = vim.cmd
 
 map("i", "jk", "<Esc>", {noremap = true})
-map("n", "<c-P>", "<cmd>lua require('fzf-lua').files()<CR>", default_opts)
+map("n", ";;", "<cmd>lua require('fzf-lua').files()<CR>", default_opts)
 
 cmd "let g:tmux_navigator_no_mappings = 1"
-map("n", "<C-h>", ":TmuxNavigateLeft<CR>", default_opts)
-map("n", "<C-j>", ":TmuxNavigateDown<CR>", default_opts)
-map("n", "<C-k>", ":TmuxNavigateUp<CR>", default_opts)
-map("n", "<C-l>", ":TmuxNavigateRight<CR>", default_opts)
-map("n", "<C-\\>", ":TmuxNavigatePrevious<CR>", default_opts)
+map("n", "<C-h>", "<C-W><C-H>", default_opts)
+map("n", "<C-j>", "<C-W><C-J>", default_opts)
+map("n", "<C-k>", "<C-W><C-K>", default_opts)
+map("n", "<C-l>", "<C-W><C-L>", default_opts)
