d469e9dadbfe8bd3ee424c15dccf557fc9f676f8
Daniel Garman <garman@github.com>
2024-03-15 05:46:35 -0400

    Kinda fix filename

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -2,5 +2,13 @@ require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'wombat',
+  },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1, 
+      }
+    }
   }
 }
