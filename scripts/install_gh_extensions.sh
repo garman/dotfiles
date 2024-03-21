16c49dd78c15827f905ae79947cabc551ac3285a
Daniel Garman <garman@github.com>
2024-03-21 17:30:16 -0400

    Sigh.

--- scripts/install_gh_extensions.sh
+++ scripts/install_gh_extensions.sh
@@ -2,7 +2,7 @@
 
 export GH_PAGER="cat"
 
-gh ext install vilmibm/gh-screenshare
+gh ext install vilmibm/gh-screensaver
 gh ext install github/gh-projects
 gh ext install samcoe/gh-repo-explore
 gh ext install yuler/gh-todo
