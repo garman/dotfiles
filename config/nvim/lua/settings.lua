85eeaf78585256a905356cda44380f4249a73dfe
Daniel Garman <garman@github.com>
2024-10-23 14:07:05 -0400

    be explicit about ale linters for ruby

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -86,6 +86,8 @@ cmd [[
   au BufWritePre * :%s/\s\+$//e
   autocmd TermOpen * setlocal listchars= nocursorline
 
+  let g:ale_linters = { 'ruby': ['rubocop', 'srb', 'rails_best_practices', 'packwerk', 'brakeman'] }
+
   au BufEnter * set fo-=c fo-=r fo-=o
   autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0
   autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml setlocal shiftwidth=2 tabstop=2
