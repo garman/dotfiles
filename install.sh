d01b8a267422f0ca65e172dade14cd5ca4118cb1
Daniel Garman <danielrgarman@gmail.com>
2025-01-05 13:54:44 -0500

    tmux work maybe

--- install.sh
+++ install.sh
@@ -36,10 +36,12 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 sudo chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 
-# Install tmux
+# Install tmux and tpm
 wget https://github.com/nelsonenzo/tmux-appimage/releases/download/3.2a/tmux.appimage
 sudo chmod u+x tmux.appimage
 sudo mv tmux.appimage /usr/local/bin/tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/tpm
 
 # Config git and gh
 ln -sf $(pwd)/gitconfig $HOME/.gitconfig
