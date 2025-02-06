07896ecc0f209a2475c07435647663c9418b9f21
Daniel Garman <danielrgarman@gmail.com>
2025-02-05 22:20:57 -0500

    add things

--- config/nvim/lua/plugins/packer.lua
+++ config/nvim/lua/plugins/packer.lua
@@ -78,4 +78,26 @@ return packer.startup(function()
   use { "fatih/vim-go", run = "vim.cmd.GoInstallBinaries()" }
   use "benmills/vim-golang-alternate"
   -- End Golang
+
+  use { "CopilotC-Nvim/CopilotChat.nvim",
+    requires = { "github/copilot.vim" }, { "nvim-lua/plenary.nvim", branch = "master" }
+  }
+
+  use {
+      'KadoBOT/nvim-spotify',
+      requires = 'nvim-telescope/telescope.nvim',
+      config = function()
+          local spotify = require'nvim-spotify'
+
+          spotify.setup {
+              -- default opts
+              status = {
+                  update_interval = 10000, -- the interval (ms) to check for what's currently playing
+                  format = '%s %t by %a' -- spotify-tui --format argument
+              }
+          }
+      end,
+      run = 'make'
+  }
+
 end)
