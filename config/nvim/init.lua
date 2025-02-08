b722541dca4b7d21479f72c4bf8b80d49877a40d
Daniel Garman <danielrgarman@gmail.com>
2025-02-07 21:12:19 -0500

    lazy

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,14 +1,168 @@
-require("plugins.packer")
-require("plugins.colorscheme")
-
-if not vim.env.HEADLESS_NEOVIM then
-  require("settings")
-  require("keymaps")
-  require("plugins.alpha-nvim")
-  require("plugins.copilot-chat")
-  require("plugins.cmp")
-  require("plugins.lualine")
-  require("plugins.gitsigns")
-  require("plugins.fzf-lua")
-  require("plugins.blink")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+  --  -- import your plugins
+    { import = "plugins" },
+  },
+  colorscheme = { "catppuccin-mocha" },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+--local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+--if not (vim.uv or vim.loop).fs_stat(lazypath) then
+--  vim.fn.system({
+--    "git",
+--    "clone",
+--    "--filter=blob:none",
+--    "https://github.com/folke/lazy.nvim.git",
+--    "--branch=stable", -- latest stable release
+--    lazypath,
+--  })
+--end
+--vim.opt.rtp:prepend(lazypath)
+--
+--require("lazy").setup({
+--	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+--	{
+--    "goolord/alpha-nvim",
+--    local dashboard = require "alpha.themes.dashboard",
+--
+--    local function footer()
+--      local datetime = os.date("%Y/%m/%d %I:%M %p", os.time() + 2 * 60 * 60 )
+--      return datetime
+--    end
+--
+--    local codespaces_bloody = {
+--      "                                                                                        ",
+--      "  ▄████▄   ▒█████  ▓█████▄ ▓█████   ██████  ██▓███   ▄▄▄       ▄████▄  ▓█████   ██████  ",
+--      " ▒██▀ ▀█  ▒██▒  ██▒▒██▀ ██▌▓█   ▀ ▒██    ▒ ▓██░  ██▒▒████▄    ▒██▀ ▀█  ▓█   ▀ ▒██    ▒  ",
+--      " ▒▓█    ▄ ▒██░  ██▒░██   █▌▒███   ░ ▓██▄   ▓██░ ██▓▒▒██  ▀█▄  ▒▓█    ▄ ▒███   ░ ▓██▄    ",
+--      " ▒▓▓▄ ▄██▒▒██   ██░░▓█▄   ▌▒▓█  ▄   ▒   ██▒▒██▄█▓▒ ▒░██▄▄▄▄██ ▒▓▓▄ ▄██▒▒▓█  ▄   ▒   ██▒ ",
+--      " ▒ ▓███▀ ░░ ████▓▒░░▒████▓ ░▒████▒▒██████▒▒▒██▒ ░  ░ ▓█   ▓██▒▒ ▓███▀ ░░▒████▒▒██████▒▒ ",
+--      " ░ ░▒ ▒  ░░ ▒░▒░▒░  ▒▒▓  ▒ ░░ ▒░ ░▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░ ▒▒   ▓▒█░░ ░▒ ▒  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░ ",
+--      "   ░  ▒     ░ ▒ ▒░  ░ ▒  ▒  ░ ░  ░░ ░▒  ░ ░░▒ ░       ▒   ▒▒ ░  ░  ▒    ░ ░  ░░ ░▒  ░ ░ ",
+--      " ░        ░ ░ ░ ▒   ░ ░  ░    ░   ░  ░  ░  ░░         ░   ▒   ░           ░   ░  ░  ░   ",
+--      " ░ ░          ░ ░     ░       ░  ░      ░                 ░  ░░ ░         ░  ░      ░   ",
+--      " ░                  ░                                         ░                         ",
+--      "                                                                                        ",
+--      "             You can checkout any time you like, but you can never leave...             ",
+--    }
+--
+--    local garman_melting = {
+--    "							     ",
+--    "   ▄████  ▄▄▄      ██▀███   ███▄ ▄███▓ ▄▄▄      ███▄    █   ",
+--    " ▒ ██▒ ▀█▒▒████▄   ▓██ ▒ ██▒▓██▒▀█▀ ██▒▒████▄    ██ ▀█   █  ",
+--    " ░▒██░▄▄▄░▒██  ▀█▄ ▓██ ░▄█ ▒▓██    ▓██░▒██  ▀█▄ ▓██  ▀█ ██▒ ",
+--    " ░░▓█  ██▓░██▄▄▄▄██▒██▀▀█▄  ▒██    ▒██ ░██▄▄▄▄██▓██▒  ▐▌██▒ ",
+--    " ░▒▓███▀▒░▒▓█   ▓██░██▓ ▒██▒▒██▒   ░██▒▒▓█   ▓██▒██░   ▓██░ ",
+--    "  ░▒   ▒  ░▒▒   ▓▒█░ ▒▓ ░▒▓░░ ▒░   ░  ░░▒▒   ▓▒█░ ▒░   ▒ ▒  ",
+--    "   ░   ░  ░ ░   ▒▒   ░▒ ░ ▒ ░  ░      ░░ ░   ▒▒ ░ ░░   ░ ▒░ ",
+--    " ░ ░   ░ ░  ░   ▒    ░░   ░ ░      ░     ░   ▒     ░   ░ ░  ",
+--    "       ░        ░     ░            ░         ░           ░  ",
+--    }
+--
+--    local garman_ghost = {
+--    "   								   ",
+--    "   ▄▄█▀▀▀█▄█                                                      ",
+--    " ▄██▀     ▀█                                                      ",
+--    " ██▀       ▀ ▄█▀██▄ ▀███▄███▀████████▄█████▄  ▄█▀██▄ ▀████████▄   ",
+--    " █▓         ██   ██   ██▀ ▀▀  ██    ██    ██ ██   ██   ██    ██   ",
+--    " █▓▄    ▀████▄███▓█   █▓      ▓█    ██    ██  ▄███▓█   █▓    ██   ",
+--    " ▀▓█▄     ███▓   ▓█   █▓      ▓█    ▓█    ██ █▓   ▓█   █▓    ▓█   ",
+--    " ▓▓▓    ▀▓█▓▓▓▓▓▓▒▓   ▓▓      ▓▓    ▓▓    ▓▓  ▓▓▓▓▒▓   ▓▓    ▓▓   ",
+--    " ▀▒▓▓     ▓▓▓▓   ▒▓   ▓▒      ▒▓    ▒▓    ▓▓ ▓▓   ▒▓   ▓▓    ▓▓   ",
+--    "   ▒▒▒ ▒ ▒▒ ▒▓▒ ▒ ▓▒▒ ▒▒▒   ▒ ▒▓▒  ▒▒▒   ▒▒▓▒▒▓▒ ▒ ▓▒▒ ▒▒▒  ▒▓▒ ▒ ",
+--    "                                                                  ",
+--    }
+--
+--    local garman_splash = {
+--    "                                       ",
+--    "  ▄▄ •  ▄▄▄· ▄▄▄  • ▌ ▄ ·.  ▄▄▄·  ▐ ▄  ",
+--    " ▐█ ▀ ▪▐█ ▀█ ▀▄ █··██ ▐███▪▐█ ▀█ •█▌▐█ ",
+--    " ▄█ ▀█▄▄█▀▀█ ▐▀▀▄ ▐█ ▌▐▌▐█·▄█▀▀█ ▐█▐▐▌ ",
+--    " ▐█▄▪▐█▐█▪ ▐▌▐█•█▌██ ██▌▐█▌▐█▪ ▐▌██▐█▌ ",
+--    " ·▀▀▀▀  ▀  ▀ .▀  ▀▀▀  █▪▀▀▀ ▀  ▀ ▀▀ █▪ ",
+--    "                                       ",
+--    }
+--
+--    local turbo_time_splash = {
+--    "                                                        ",
+--    " ▄▄▄▄▄▄• ▄▌▄▄▄  ▄▄▄▄·            ▄▄▄▄▄▪  • ▌ ▄ ·. ▄▄▄ . ",
+--    " •██  █▪██▌▀▄ █·▐█ ▀█▪▪          •██  ██ ·██ ▐███▪▀▄.▀· ",
+--    "  ▐█.▪█▌▐█▌▐▀▀▄ ▐█▀▀█▄ ▄█▀▄       ▐█.▪▐█·▐█ ▌▐▌▐█·▐▀▀▪▄ ",
+--    "  ▐█▌·▐█▄█▌▐█•█▌██▄▪▐█▐█▌.▐▌      ▐█▌·▐█▌██ ██▌▐█▌▐█▄▄▌ ",
+--    "  ▀▀▀  ▀▀▀ .▀  ▀·▀▀▀▀  ▀█▄▀▪      ▀▀▀ ▀▀▀▀▀  █▪▀▀▀ ▀▀▀  ",
+--    "                                                        ",
+--    "             Has that ever happened to you?             ",
+--    }
+--
+--    local fifty_five_splash = {
+--    "                                             ",
+--    " ·▄▄▄▪  ·▄▄▄▄▄▄▄▄ ▄· ▄▌    ·▄▄▄▪   ▌ ▐·▄▄▄ . ",
+--    " ▐▄▄·██ ▐▄▄·•██  ▐█▪██▌    ▐▄▄·██ ▪█·█▌▀▄.▀· ",
+--    " ██▪ ▐█·██▪  ▐█.▪▐█▌▐█▪    ██▪ ▐█·▐█▐█•▐▀▀▪▄ ",
+--    " ██▌.▐█▌██▌. ▐█▌· ▐█▀·.    ██▌.▐█▌ ███ ▐█▄▄▌ ",
+--    " ▀▀▀ ▀▀▀▀▀▀  ▀▀▀   ▀ •     ▀▀▀ ▀▀▀. ▀   ▀▀▀  ",
+--    "                                             ",
+--    "             I'm doing a thing!              ",
+--    }
+--
+--    local nachos_splash = {
+--    "                                   ",
+--    "  ▐ ▄  ▄▄▄·  ▄▄·  ▄ .▄      .▄▄ ·  ",
+--    " •█▌▐█▐█ ▀█ ▐█ ▌▪██▪▐█▪     ▐█ ▀.  ",
+--    " ▐█▐▐▌▄█▀▀█ ██ ▄▄██▀▐█ ▄█▀▄ ▄▀▀▀█▄ ",
+--    " ██▐█▌▐█ ▪▐▌▐███▌██▌▐▀▐█▌.▐▌▐█▄▪▐█ ",
+--    " ▀▀ █▪ ▀  ▀ ·▀▀▀ ▀▀▀ · ▀█▄▀▪ ▀▀▀▀  ",
+--    "                                   ",
+--    "    What are you talking about?    "
+--    }
+--
+--    local dashboard_header_options = {
+--      turbo_time_splash,
+--      fifty_five_splash,
+--      nachos_splash,
+--    }
+--    math.randomseed(os.clock()*100000000000)
+--    math.random(); math.random(); math.random()
+--    local header_choice = math.random(#dashboard_header_options)
+--
+--    dashboard.section.header.val = dashboard_header_options[header_choice]
+--
+--    dashboard.section.buttons.val = {
+--      dashboard.button("e", "New file", ":ene <BAR> startinsert<CR>"),
+--      dashboard.button("f", "Find file", ":FzfLua files<CR>"),
+--      dashboard.button("r", "Recent files", ":FzfLua oldfiles<CR>"),
+--      dashboard.button("s", "Settings", ":e $MYVIMRC<CR>"),
+--      dashboard.button("u", "Update plugins", ":PackerUpdate<CR>"),
+--      dashboard.button("q", "Quit", ":qa<CR>"),
+--    }
+--
+--    dashboard.section.footer.val = footer()
+--
+--    require("alpha").setup(dashboard.config)
+--})
+--
+require("settings")
+require("keymaps")
---
+++ config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,154 @@
+local function footer()
+  datetime = os.date("%Y/%m/%d %I:%M %p", os.time() + 2 * 60 * 60 )
+  return datetime
+end
+
+local footer_opts = {
+  type = "text",
+  val = footer,
+  opts = { position = "center", hl = "number" }
+}
+
+local turbo_time_splash = {
+  type = "text",
+  val = {
+    "                                                        ",
+    " ▄▄▄▄▄▄• ▄▌▄▄▄  ▄▄▄▄·            ▄▄▄▄▄▪  • ▌ ▄ ·. ▄▄▄ . ",
+    " •██  █▪██▌▀▄ █·▐█ ▀█▪▪          •██  ██ ·██ ▐███▪▀▄.▀· ",
+    "  ▐█.▪█▌▐█▌▐▀▀▄ ▐█▀▀█▄ ▄█▀▄       ▐█.▪▐█·▐█ ▌▐▌▐█·▐▀▀▪▄ ",
+    "  ▐█▌·▐█▄█▌▐█•█▌██▄▪▐█▐█▌.▐▌      ▐█▌·▐█▌██ ██▌▐█▌▐█▄▄▌ ",
+    "  ▀▀▀  ▀▀▀ .▀  ▀·▀▀▀▀  ▀█▄▀▪      ▀▀▀ ▀▀▀▀▀  █▪▀▀▀ ▀▀▀  ",
+    "                                                        ",
+    "             Has that ever happened to you?             ",
+  },
+  opts = { position = "center", hl = "Type" },
+}
+
+local fifty_five_splash = {
+  type = "text",
+  val = {
+    "                                             ",
+    " ·▄▄▄▪  ·▄▄▄▄▄▄▄▄ ▄· ▄▌    ·▄▄▄▪   ▌ ▐·▄▄▄ . ",
+    " ▐▄▄·██ ▐▄▄·•██  ▐█▪██▌    ▐▄▄·██ ▪█·█▌▀▄.▀· ",
+    " ██▪ ▐█·██▪  ▐█.▪▐█▌▐█▪    ██▪ ▐█·▐█▐█•▐▀▀▪▄ ",
+    " ██▌.▐█▌██▌. ▐█▌· ▐█▀·.    ██▌.▐█▌ ███ ▐█▄▄▌ ",
+    " ▀▀▀ ▀▀▀▀▀▀  ▀▀▀   ▀ •     ▀▀▀ ▀▀▀. ▀   ▀▀▀  ",
+    "                                             ",
+    "             I'm doing a thing!              ",
+  },
+  opts = { position = "center", hl = "Type" },
+}
+
+local nachos_splash = {
+  type = "text",
+  val = {
+    "                                   ",
+    "  ▐ ▄  ▄▄▄·  ▄▄·  ▄ .▄      .▄▄ ·  ",
+    " •█▌▐█▐█ ▀█ ▐█ ▌▪██▪▐█▪     ▐█ ▀.  ",
+    " ▐█▐▐▌▄█▀▀█ ██ ▄▄██▀▐█ ▄█▀▄ ▄▀▀▀█▄ ",
+    " ██▐█▌▐█ ▪▐▌▐███▌██▌▐▀▐█▌.▐▌▐█▄▪▐█ ",
+    " ▀▀ █▪ ▀  ▀ ·▀▀▀ ▀▀▀ · ▀█▄▀▪ ▀▀▀▀  ",
+    "                                   ",
+    "    What are you talking about?    ",
+  },
+  opts = { position = "center", hl = "Type" },
+}
+
+
+local random_header = function()
+  dashboard_header_options = {
+    turbo_time_splash,
+    --fifty_five_splash,
+    --nachos_splash,
+  }
+  math.randomseed(os.clock()*100000000000)
+  math.random(); math.random(); math.random()
+  header_choice = math.random(#dashboard_header_options)
+  return dashboard_header_options[header_choice]
+end
+
+local function button(btn, txt, keybind)
+  local sc = btn:gsub("%s", ""):gsub("SPC", "<leader>")
+
+  local opts = {
+    position = "center",
+    shortcut = sc,
+    cursor = 0,
+    width = 50,
+    align_shortcut = "right",
+    hl_shortcut = "Keyword",
+  }
+
+  if keybind then
+    opts.keymap = { "n", sc, keybind, { noremap = true, silent = true, nowait = true } }
+  end
+
+  local function on_press()
+    local key = vim.api.nvim_replace_termcodes(sc .. "<Ignore>", true, false, true)
+    vim.api.nvim_feedkeys(key, "t", false)
+  end
+
+  return {
+    type = "button",
+    val = txt,
+    on_press = on_press,
+    opts = opts,
+  }
+end
+
+local buttons = {
+  type = "group",
+  val = {
+    button("e", "New file", ":ene <BAR> startinsert<CR>"),
+    button("f", "Find file", ":FzfLua files<CR>"),
+    button("r", "Recent files", ":FzfLua oldfiles<CR>"),
+    button("s", "Settings", ":e $MYVIMRC<CR>"),
+    button("u", "Update plugins", ":PackerUpdate<CR>"),
+    button("q", "Quit", ":qa<CR>"),
+  },
+  opts = {
+    spacing = 0,
+  },
+}
+
+
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+
+    config = function()
+      require("catppuccin").setup {
+        background = {
+          dark = "mocha"
+        },
+        styles = {
+          comments = { "italic" },
+          conditionals = {},
+        }
+      }
+      vim.cmd([[colorscheme blue]])
+    end
+  },
+
+  {
+    'goolord/alpha-nvim',
+    lazy = vim.fn.argc(-1) ~= 0,
+    priority = 1000,
+    opts = {
+      layout = {
+        { type = "padding", val = 2 },
+        random_header(),
+        { type = "padding", val = 2 },
+        buttons,
+        { type = "padding", val = 1 },
+        footer_opts,
+      },
+      opts = {
+        margin = 50,
+      },
+    }
+  }
+}
