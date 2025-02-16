c93323f9c8a870785d9348ea7a2db8b1d65a23b6
Daniel Garman <danielrgarman@gmail.com>
2025-02-16 17:00:53 -0500

    touch config

--- install.sh
+++ install.sh
@@ -54,6 +54,8 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
+mkdir ~/.config/lazygit
+touch ~/.config/lazygit/config.yml
 
 # Config git and gh
 ln -sf $(pwd)/gitconfig $HOME/.gitconfig
