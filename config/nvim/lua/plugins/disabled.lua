88a38224650bd95b2723e235e2381176d77d0ce2
Daniel Garman <danielrgarman@gmail.com>
2025-02-17 03:11:58 -0500

    Disable treesitter

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,3 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
 }
