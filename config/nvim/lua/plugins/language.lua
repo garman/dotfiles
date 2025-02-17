30268993c409ae6673402c7ec6c670494892018b
Daniel Garman <danielrgarman@gmail.com>
2025-02-17 04:48:00 -0500

    add language

--- config/nvim/lua/plugins/language.lua
+++ config/nvim/lua/plugins/language.lua
@@ -1,15 +1,14 @@
 return {
   {
     "fatih/vim-go",
-    lazy = true,
     config = function()
       vim.cmd.GoInstallBinaries()
     end,
   },
   { "benmills/vim-golang-alternate" },
 
-  { "tpope/vim-rails" },
-  { "vim-ruby/vim-ruby" },
+  { "tpope/vim-rails"},
+  { "vim-ruby/vim-ruby"},
   { "leafgarland/typescript-vim" },
   { "peitalin/vim-jsx-typescript" },
   { "OXY2DEV/markview.nvim" },
