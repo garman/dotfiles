f02b0fe08184b885ae251571c99e2ed86f95dc1f
Daniel Garman <danielrgarman@gmail.com>
2025-02-17 03:15:15 -0500

    languages lazy go

--- config/nvim/lua/plugins/language.lua
+++ config/nvim/lua/plugins/language.lua
@@ -1,14 +1,15 @@
 return {
   {
     "fatih/vim-go",
+    lazy = true,
     config = function()
       vim.cmd.GoInstallBinaries()
     end,
   },
   { "benmills/vim-golang-alternate" },
 
-  { "tpope/vim-rails"},
-  { "vim-ruby/vim-ruby"},
+  { "tpope/vim-rails" },
+  { "vim-ruby/vim-ruby" },
   { "leafgarland/typescript-vim" },
   { "peitalin/vim-jsx-typescript" },
   { "OXY2DEV/markview.nvim" },
